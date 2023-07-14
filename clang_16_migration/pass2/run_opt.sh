e8a5bb43ec67566af3e172e12054f32a5022f324
Andy Ragusa <aragusa@cisco.com>
2023-07-14 14:12:24 -0700

    blah

---
+++ clang_16_migration/pass2/run_opt.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clang-16 -S -emit-llvm -O0 -Xclang -disable-O0-optnone ../../testing/test.c
+
+opt-16 -S --load-pass-plugin libclambcc/libclambcc.so --passes=my-pass test.ll -o test.t.ll
+
+
