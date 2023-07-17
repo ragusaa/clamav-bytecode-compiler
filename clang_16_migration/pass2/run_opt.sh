6a63661e62af0283acf84e4f24f0f99ac5e87b74
Andy Ragusa <aragusa@cisco.com>
2023-07-17 12:56:20 -0700

    blah

--- clang_16_migration/pass2/run_opt.sh
+++ clang_16_migration/pass2/run_opt.sh
@@ -9,7 +9,25 @@ clang-16 -S -emit-llvm -O0 -Xclang -disable-O0-optnone ../../testing/test.c
 
 #opt-16 -S --load-pass-plugin libclambcc/libclambcc.so --passes="my-module-pass,my-function-pass" test.ll -o test.t.ll
 
-opt-16 -S --load-pass-plugin libclambcc/libclambcc.so --passes=my-module-pass test.ll -o test.t.ll
+#opt-16 -S --load-pass-plugin libclambcc/libclambcc.so --passes=my-module-pass test.ll -o test.t.ll
 #opt-16 -S --load-pass-plugin libclambcc/libclambcc.so --passes=my-function-pass test.ll -o test.t.ll
+
+
+
+#opt-16 -S --load-pass-plugin libclambcc/libclambcc.so --load-pass-plugin ./libclambcc/MyModulePass/libclambcc_mymodulepass.so --passes=my-module-pass test.ll -o test.t.ll
+
+
+
+#opt-16 -S \
+#    --load-pass-plugin libclambcc/libclambcc.so \
+#    --load-pass-plugin libclambcc/MyModulePass/libclambcc_mymodulepass.so \
+#    --passes="my-module-pass,my-function-pass" test.ll -o test.t.ll
+#
+
+opt-16 -S \
+    --load-pass-plugin libclambcc/MyModulePass/libclambcc_mymodulepass.so \
+    --load-pass-plugin libclambcc/MyModulePass2/libclambcc_mymodulepass2.so \
+    --passes="my-module-pass,my-module-pass2" test.ll -o test.t.ll
+
 
 
