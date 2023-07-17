30ef48cdd9184f3c1b5ec14bb57d1ca2f740a7b6
Andy Ragusa <aragusa@cisco.com>
2023-07-17 11:57:19 -0700

    blah

--- clang_16_migration/pass2/run_opt.sh
+++ clang_16_migration/pass2/run_opt.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+
+#might be useful
+#https://stackoverflow.com/questions/67206238/how-to-define-and-read-cli-arguments-for-an-llvm-pass-with-the-new-pass-manager
+
+
 clang-16 -S -emit-llvm -O0 -Xclang -disable-O0-optnone ../../testing/test.c
 
-opt-16 -S --load-pass-plugin libclambcc/libclambcc.so --passes=my-function-pass test.ll -o test.t.ll
+#opt-16 -S --load-pass-plugin libclambcc/libclambcc.so --passes="my-module-pass,my-function-pass" test.ll -o test.t.ll
+
+opt-16 -S --load-pass-plugin libclambcc/libclambcc.so --passes=my-module-pass test.ll -o test.t.ll
+#opt-16 -S --load-pass-plugin libclambcc/libclambcc.so --passes=my-function-pass test.ll -o test.t.ll
 
 
