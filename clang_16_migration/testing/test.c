1491d57e0054c4926f90767ff0e6e8a322b6b8e2
Andy Ragusa <aragusa@cisco.com>
2023-07-17 13:56:10 -0700

    blah

--- clang_16_migration/testing/test.c
+++ clang_16_migration/testing/test.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 
-void func(){
+void func(int val){
     printf("%s::%d\n", __FUNCTION__, __LINE__);
 }
 
 int main(int argc, char ** argv){
 
     if (argc > 2){
-        func();
+        func(1);
     }
 
     return 0;
