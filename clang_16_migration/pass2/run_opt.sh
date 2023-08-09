8e5becc8b211cdf3b68ecd558ba59331c944c833
Andy Ragusa <aragusa@cisco.com>
2023-08-09 13:53:35 -0700

    lbah

--- clang_16_migration/pass2/run_opt.sh
+++ clang_16_migration/pass2/run_opt.sh
@@ -50,9 +50,8 @@ clang-16 -S -emit-llvm -O0 -Xclang -disable-O0-optnone ../../testing/test.c
 opt-16 -S \
     --load-pass-plugin libclambcc/ClamBCRemoveUndefs/libclambcremoveundefs.so \
     --load-pass-plugin libclambcc/ClamBCPreserveABIs/libclambcpreserveabis.so \
-    --passes -mem2reg \
-    --passes="clambc-remove-undefs,clambc-preserve-abis" \
-    -O3 \
+    --passes="-mem2reg"\
+    --passes="clambc-remove-undefs,clambc-preserve-abis,default<O3>" \
     test.ll -o test.t.ll
 
 
