87661227023e389feb52f6c23b48bbb3d5a50730
Andy Ragusa <aragusa@cisco.com>
2023-10-27 06:08:44 -0700

    blah

--- libclambcc/ClamBCConvertIntrinsics/ClamBCConvertIntrinsics.cpp
+++ libclambcc/ClamBCConvertIntrinsics/ClamBCConvertIntrinsics.cpp
@@ -1,15 +1,19 @@
 
 #include <llvm/Pass.h>
-#include "llvm/IR/Function.h"
-#include "llvm/IR/Module.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/Support/raw_ostream.h"
+#include <llvm/IR/Function.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/Support/raw_ostream.h>
 
-#include "llvm/IR/DerivedTypes.h"
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/PassManager.h>
+#include <llvm/Passes/PassBuilder.h>
+#include <llvm/Passes/PassPlugin.h>
 
 #include <llvm/IR/Dominators.h>
+#include <llvm/IR/Constants.h>
 
-#include "llvm/IR/LegacyPassManager.h"
+//#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
 #include "Common/clambc.h"
@@ -18,21 +22,28 @@
 
 using namespace llvm;
 
-namespace
-{
+namespace ClamBCConvertIntrinsics {
 
-class ConvertIntrinsics : public ModulePass
+#if 0
+class ClamBCConvertIntrinsics : public ModulePass
+#else
+class ClamBCConvertIntrinsics : public PassInfoMixin<ClamBCConvertIntrinsics > 
+#endif
 {
 
   public:
     static char ID;
 
-    ConvertIntrinsics()
-        : ModulePass(ID) {}
+    ClamBCConvertIntrinsics()
+        /* : ModulePass(ID) */ {}
 
-    virtual ~ConvertIntrinsics() {}
+    virtual ~ClamBCConvertIntrinsics() {}
 
+#if 0
     virtual bool runOnModule(Module& mod)
+#else
+    PreservedAnalyses run(Module & mod, ModuleAnalysisManager & MAM)
+#endif
     {
         bChanged = false;
         pMod     = &mod;
@@ -46,7 +57,15 @@ class ConvertIntrinsics : public ModulePass
             delLst[i]->eraseFromParent();
         }
 
+#if 0
         return bChanged;
+#else
+        if (bChanged){
+            return PreservedAnalyses::none();
+        }
+
+        return PreservedAnalyses::all();
+#endif
     }
 
   protected:
@@ -67,7 +86,8 @@ class ConvertIntrinsics : public ModulePass
     {
         for (auto i = pBB->begin(), e = pBB->end(); i != e; i++) {
             if (CallInst* pci = llvm::dyn_cast<CallInst>(i)) {
-                if (Function* f = llvm::dyn_cast<Function>(pci->getCalledValue())) {
+                Function * f = pci->getCalledFunction();
+                if (nullptr != f) {
                     if ("llvm.memset.p0i8.i64" == f->getName()) {
                         convertMemset(pci);
                     }
@@ -81,32 +101,43 @@ class ConvertIntrinsics : public ModulePass
         std::vector<Value*> args;
         Type* i32Ty = Type::getInt32Ty(pMod->getContext());
 
-        for (size_t i = 0; i < pci->getNumArgOperands(); i++) {
+#if 0
+        for (size_t i = 0; i < pci->getNumArgOperands(); i++)
+#else
+        for (size_t i = 0; i < pci->arg_size(); i++)
+#endif
+        {
             Value* pv = pci->getArgOperand(i);
             if (2 == i) {
                 if (ConstantInt* ci = llvm::dyn_cast<ConstantInt>(pv)) {
                     pv = ConstantInt::get(i32Ty, ci->getValue().getLimitedValue());
                 } else {
-                    pv = CastInst::CreateTruncOrBitCast(pv, i32Ty, "ConvertIntrinsics_trunc_", pci);
+                    pv = CastInst::CreateTruncOrBitCast(pv, i32Ty, "ClamBCConvertIntrinsics_trunc_", pci);
                 }
             }
 
             args.push_back(pv);
         }
 
+#if 0
         Constant* f = getNewMemset();
         CallInst::Create(getMemsetType(), f, args, "", pci);
+#else
+        FunctionCallee * f = getNewMemset();
+        CallInst::Create(*f, args, "", pci);
+#endif
         delLst.push_back(pci);
     }
 
-    llvm::Constant* getNewMemset()
+    llvm::FunctionCallee* getNewMemset()
     {
-        static llvm::Constant* ret = nullptr;
+        static llvm::FunctionCallee* ret = nullptr;
 
         if (nullptr == ret) {
 
             FunctionType* retType = getMemsetType();
-            ret                   = pMod->getOrInsertFunction("llvm.memset.p0i8.i32", retType);
+            static llvm::FunctionCallee fc = pMod->getOrInsertFunction("llvm.memset.p0i8.i32", retType);
+            ret                   = &fc;
 
             assert(ret && "Could not get memset");
         }
@@ -129,7 +160,32 @@ class ConvertIntrinsics : public ModulePass
 
 } // end of anonymous namespace
 
-char ConvertIntrinsics::ID = 0;
-static RegisterPass<ConvertIntrinsics> XX("clambc-convert-intrinsics", "Convert Intrinsics to 32-bit",
+#if 0
+char ClamBCConvertIntrinsics::ID = 0;
+static RegisterPass<ClamBCConvertIntrinsics> XX("clambc-convert-intrinsics", "Convert Intrinsics to 32-bit",
                                           false /* Only looks at CFG */,
                                           false /* Analysis Pass */);
+#else
+
+// This part is the new way of registering your pass
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
+llvmGetPassPluginInfo() {
+  return {
+    LLVM_PLUGIN_API_VERSION, "ClamBCConvertIntrinsics", "v0.1",
+    [](PassBuilder &PB) {
+      PB.registerPipelineParsingCallback(
+        [](StringRef Name, ModulePassManager &FPM,
+        ArrayRef<PassBuilder::PipelineElement>) {
+          if(Name == "clambc-convert-intrinsics"){
+            FPM.addPass(ClamBCConvertIntrinsics::ClamBCConvertIntrinsics());
+            return true;
+          }
+          return false;
+        }
+      );
+    }
+  };
+}
+
+
+#endif
